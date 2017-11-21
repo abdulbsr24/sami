3b117ba8c5110a7711a810569a88885604615bdd
abdul <abdulbsr24@gmail.com>
2017-11-20 16:06:29 -0800

    hi

---
+++ samiKhan/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="samiKhan.Global" Language="C#" %>
---
+++ samiKhan/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(samiKhan.Startup))]
+namespace samiKhan
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
